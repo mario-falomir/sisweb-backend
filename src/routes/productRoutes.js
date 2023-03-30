5e1f855d235b01d1b03d2454711e21101370bda0
m <m@tag.local>
2023-03-30 13:23:32 -0700

    added routes

--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,26 +1,12 @@
 const express = require("express");
+const controler = require("../controlers/productControler");
 
 const productRouter = express.Router();
 
-productRouter.get('/', (req, res) => {
- res.send('Get a list of products')
-});
-
-productRouter.get('/:id', (req, res) => {
- res.send(`Get the product ${req.params.id}`)
-});
-
-productRouter.post('/', (req, res) => {
- res.send(`Create a new product with ID: ${req.body.id}`)
-});
-
-productRouter.patch('/:id', (req, res) => {
- res.send(`Update the product ${req.params.id} with the values of ${req.body.name}, ${req.body.price} and
-${req.body.qty}`)
-});
-
-productRouter.delete('/', (req, res) => {
- res.send(`Deleting the product ${req.body.id}`)
-});
+productRouter.get('/', controler.getAllProducts);
+productRouter.get('/:id', controler.getProductById);
+productRouter.post('/', controler.createProduct);
+productRouter.patch('/:id', controler.updateProduct);
+productRouter.delete('/', controler.deleteProduct);
 
 module.exports = productRouter;
